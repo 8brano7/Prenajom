41d82eb5808536119549c2f72f4ec3da24d782c5
8brano7 <brano87@gmail.com>
2017-05-19 17:02:49 +0200

    Doplnený text "O nás"

--- application/views/about.php
+++ application/views/about.php
@@ -1,1 +1,25 @@
-<h1>Bude doplnené</h1>
+<HR>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            color: black;
+        }
+
+        h1 {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+
+<h1>V krátkosti o našej spoločnosti</h1>
+<p>Zaoberáme sa prenajímáním sídiel pre existujúce i novo zakladané spoločnosti a podnikateľov
+    hľadajúcich reprezentatívnu adresu s profesionálnym administratívnym zázemím.
+    Niekoľkoročné skúseností našich obchodných expertov z nás robia tradičného partnera v oblasti.
+    Máme nadštandardné vzťahy so spoločnosťami venujúcimi sa daňovému a finančnému plánovaniu a takisto
+    vám vieme odporučiť odborníkov na zakladanie či predaj spoločností s ohľadom aj na zahraničné právne normy.</p>
+
+</body>
+</html>
